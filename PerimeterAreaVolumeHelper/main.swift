b23083cad6d614e662f129675d84b5c1cb3880b1
Devon Hanses <dhansen@lcs.on.ca>
2021-01-29 12:26:32 -0500

    End of Friday Jan 29 class added switch statement to code. Nest steps start coding functions

--- PerimeterAreaVolumeHelper/main.swift
+++ PerimeterAreaVolumeHelper/main.swift
@@ -8,44 +8,91 @@
 
 import Foundation
 
-print("Hello, World!")
+//print("Hello, World!")
+//
+//// MARK: Functions
+//
+///// Finds the area of a rectangle
+///// - Parameter length: length of rectangle
+///// - Parameter width: width of rectangle
+///// - Returns: area of the rectangle
+//func areaOfRectangle(length: Double, width: Double) -> Double {
+//
+//    return length * width
+//
+//}
 
-// MARK: Functions
+///// Finds the area of the circle
+///// - Parameter radius: the straight line distance form the center of the circle to the outer edge.
+///// - Returns: area of the circle
+//func areaOfCircle(radius: Double) -> Double {
+//    return Double.pi * pow(radius, 2.0)
+//}
+//
+//
+//// MARK: Input
+//let givenLength = 10.0 //set to double using "type inference" (swift guessest he type
+//let givenWidth: Double = 2 // set to a double usign typr annotation (tell swift what to do)
+//
+//
+//
+//
+////MARK: Process
+//let area = areaOfRectangle(length: Double(givenLength), width: givenWidth)
+////MARK: Ouput
+//print("The area is \(area)") // Example of "string interpolation" (print the contents of a variable)
 
-/// Finds the area of a rectangle
-/// - Parameter length: length of rectangle
-/// - Parameter width: width of rectangle
-/// - Returns: area of the rectangle
-func areaOfRectangle(length: Double, width: Double) -> Double {
-    
-    return length * width
-    
+
+//INPUT
+
+print("What shape would you likt to find the area of? (CY/SP/CN/SB/RP/TP)")
+print("CY: Cylinder")
+print("SP: Sphere")
+print("CN: Cone")
+print("RP: Rectangular Prisim")
+print("TP: Triangular Prisim")
+print("enter your choice here:  ")
+var shape = readLine()!
+
+switch shape {
+   case "CY":
+     print("Cylinder")
+       //print("What is the radius:  ")
+          //
+          //var CYr = readLine()!
+          //print("What is the height:  ")
+          //
+          //func surfaceAreaCylinder(radius: Double, height: Double)
+          //return 2 * Double.pi * pow(radius, 2.0) + 2 * Double.pi * radius * height
+   case "SP":
+     print("Sphere")
+   case "CN":
+      print("Cone")
+   case "RP":
+      print("Rectangular Prisim")
+   case "TP":
+      print("Triangular Prisim")
+   default:
+       break
 }
 
-/// Finds the area of the circle
-/// - Parameter radius: the straight line distance form the center of the circle to the outer edge.
-/// - Returns: area of the circle
-func areaOfCircle(radius: Double) -> Double {
-    return Double.pi * pow(radius, 2.0)
+   
 }
+//MARK: Cylinder
+//Cylinder              (CY) A=2(pi)r^2 + 2(pi)rh  (r, h)
 
+//MARK: Sphere
+//Sphere                (SP) A=4(pi)r^2            (r)
 
-// MARK: Input
-let givenLength = 10.0 //set to double using "type inference" (swift guessest he type
-let givenWidth: Double = 2 // set to a double usign typr annotation (tell swift what to do)
+//MARK: Cone
+//Cone                  (CN) A=(pi)r^2 + (pi)rs    (r, s)
 
+//MARK: Square-Based Pyramids
+//Square-Based Pyramids (SB) A=b^2 + 2bs           (b, s)
 
+//MARK: Rectangular Prisim
+//Recangular Prisim     (RP) A=2(wh + lw + lh)     (w, h, l)
 
-
-//MARK: Process
-let area = areaOfRectangle(length: Double(givenLength), width: givenWidth)
-//MARK: Ouput
-print("The area is \(area)") // Example of "string interpolation" (print the contents of a variable)
-
-
-
-
-//Cylinder
-
-
+//MARK: Triangular Prism
+//Triangular Prism      (TP) A=bl + ah + bh + ch   (b, l, a, c, h)
 
