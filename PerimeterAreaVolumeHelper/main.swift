40a4397ff5edd02f8957dbae7ffb8daa8c9a8e51
Devon Hanses <dhansen@lcs.on.ca>
2021-02-01 11:16:54 -0500

    Before Dylan helps me mess it up

--- PerimeterAreaVolumeHelper/main.swift
+++ PerimeterAreaVolumeHelper/main.swift
@@ -56,43 +56,121 @@ var shape = readLine()!
 
 switch shape {
    case "CY":
+    
+    //MARK: Cylinder
+    //Cylinder              (CY) A=2(pi)r^2 + 2(pi)rh  (r, h)
+    
      print("Cylinder")
-       //print("What is the radius:  ")
-          //
-          //var CYr = readLine()!
-          //print("What is the height:  ")
-          //
-          //func surfaceAreaCylinder(radius: Double, height: Double)
-          //return 2 * Double.pi * pow(radius, 2.0) + 2 * Double.pi * radius * height
+       print("What is the radius?:  ")
+            var radius = readLine()!
+          
+     print("What is the height:  ")
+          var height = readLine()!
+     
+     func surfaceAreaCylinder(radius: Double, height: Double) {
+          return 2 * Double.pi * pow(radius, 2.0) + 2 * Double.pi * radius * height
+    }
    case "SP":
+    
+    //MARK: Sphere
+    //Sphere                (SP) A=4(pi)r^2            (r)
+
      print("Sphere")
-   case "CN":
+        print("What is the radius?:  ")
+            var radius = readLine()!
+    
+     func surfaceAreaSphere(radius: Double) {
+        return 4 * Double.pi * pow(radius, 2.0)
+    }
+    case "CN":
+    
+    //MARK: Cone
+    //Cone                  (CN) A=(pi)r^2 + (pi)rs    (r, s)
+    
       print("Cone")
-   case "RP":
+    print("What is the radius?:  ")
+            var radius = readLine()!
+       
+      print("What is the side length?:  ")
+            var side = readLine()!
+      
+      func surfaceAreaCone(radius: Double, side: Double) {
+        return Double.pi * pow(radius, 2.0) + Double.pi * radius * side
+       }
+    
+    
+    case "SB":
+    
+    //MARK: Square-Based Pyramids
+    //Square-Based Pyramids (SB) A=b^2 + 2bs           (b, s)
+        print("Square-Based Pyramids")
+            print("What is the base?:  ")
+                    var base = readLine()!
+               
+              print("What is the side length?:  ")
+                    var side = readLine()!
+              
+              func surfaceAreaCone(base: Double, side: Double) {
+                return pow(base, 2.0) + 2 * base * side
+               }
+
+    case "RP":
+    
+    //MARK: Rectangular Prisim
+    //Recangular Prisim     (RP) A=2(wh + lw + lh)     (w, h, l)
+
+    
       print("Rectangular Prisim")
+                   print("What is the width?:  ")
+                           var width = readLine()!
+                      
+                     print("What is the length?:  ")
+                           var length = readLine()!
+                    
+                    print("What is the height?:  ")
+                            var height = readLine()!
+                     
+      func surfaceAreaCone(width: Double, height: Double, length: Double) {
+                       return 2 * (width * height + length * width + length * height)
+                      }
+
+    
    case "TP":
+    
+    //MARK: Triangular Prism
+    //Triangular Prism      (TP) A=bl + ah + bh + ch   (b, l, a, c, h)
       print("Triangular Prisim")
+                     print("What is the bValue?:  ")
+                             var b = readLine()!
+                        
+                       print("What is the lValue?:  ")
+                             var l = readLine()!
+                      
+                      print("What is the aValue?:  ")
+                              var a = readLine()!
+                       
+                        print("What is the cValue?:  ")
+                               var c = readLine()!
+                          
+                         print("What is the hValue?:  ")
+                               var h = readLine()!
+                    
+                        bl + ah + bh + ch
+      func surfaceAreaCone(b: Double, l: Double, a: Double, c: Double, h: Double) {
+                         return b * l + a * h + b * h + c * h
+                        }
+
    default:
        break
 }
 
    
-}
-//MARK: Cylinder
-//Cylinder              (CY) A=2(pi)r^2 + 2(pi)rh  (r, h)
 
-//MARK: Sphere
-//Sphere                (SP) A=4(pi)r^2            (r)
 
-//MARK: Cone
-//Cone                  (CN) A=(pi)r^2 + (pi)rs    (r, s)
 
-//MARK: Square-Based Pyramids
-//Square-Based Pyramids (SB) A=b^2 + 2bs           (b, s)
 
-//MARK: Rectangular Prisim
-//Recangular Prisim     (RP) A=2(wh + lw + lh)     (w, h, l)
 
-//MARK: Triangular Prism
-//Triangular Prism      (TP) A=bl + ah + bh + ch   (b, l, a, c, h)
+
+
+
 
